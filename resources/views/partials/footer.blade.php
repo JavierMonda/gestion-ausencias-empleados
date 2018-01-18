267b29d2a938d5b5eea2d2a84b741815bcadec9e
JavierMonda <javier.raidpc@gmail.com>
2018-01-18 12:51:28 +0000

    Footer actualizado

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,26 +1,11 @@
 <footer class="footer">
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-md-5">
-        <h5>Oasis Park Fuerteventura</h5>
-        <p>Para cualquier incidencia o sugerencia, contacta con el Departamento de Informática a través del siguiente formulario: </p>
+      <div class="col-sm-6">
+        <p>GAE - Gestión de Ausencias de Empleados</p>
       </div>
-      <div class="col-md-2">
-        <h5 class="text-md-right"></h5>
-        <hr>
-      </div>
-      <div class="col-md-5">
-        <form method="POST" action="mailto:informatica@fuerteventuraoasispark.com">
-          <fieldset class="form-group">
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Ingrese el Email">
-          </fieldset>
-          <fieldset class="form-group">
-            <textarea class="form-control" id="exampleMessage" placeholder="Mensaje"></textarea>
-          </fieldset>
-          <fieldset class="form-group text-xs-right">
-            <button type="button" class="btn btn-secondary-outline btn-lg">Enviar</button>
-          </fieldset>
-        </form>
+      <div class="col-sm-6 text-right">
+        <p>Departamento de Informática - Fuerteventura Oasis Park</p>
       </div>
     </div>
   </div>
